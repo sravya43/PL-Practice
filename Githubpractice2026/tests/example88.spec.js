1064faf53c270e963a3fd2a465fb56448d12f743
sravya43 <42236619+sravya43@users.noreply.github.com>
2026-02-06 18:35:45 +0530

    Update expected title in example88.spec.js

--- Githubpractice2026/tests/example88.spec.js
+++ Githubpractice2026/tests/example88.spec.js
@@ -5,7 +5,7 @@ test('has title', async ({ page }) => {
   await page.goto('https://playwright.dev/');
 
   // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright-test99/);
+  await expect(page).toHaveTitle(/Playwright-test999/);
 });
 
 test('get started link', async ({ page }) => {
